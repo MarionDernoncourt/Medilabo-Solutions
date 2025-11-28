d5573addb3e2063b79c22a1aaf9117942915df1a
MarionDernoncourt <marion.dernon@gmail.com>
2025-11-28 15:55:31 +0100

    first commit

---
+++ ms-patient/ms-patient/src/main/java/com/java/medilabo/ms/patient/ms_patient/MicroservicePatientApplication.java
@@ -0,0 +1,13 @@
+package com.java.medilabo.ms.patient.ms_patient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicePatientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicePatientApplication.class, args);
+	}
+
+}
---
+++ ms-patient/ms-patient/src/test/java/com/java/medilabo/ms/patient/ms_patient/MicroservicePatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.medilabo.ms.patient.ms_patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicePatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
