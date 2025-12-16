118bdbeb2f9c35563f18fcb2e0ac1ddfc7dd6d8d
MarionDernoncourt <marion.dernon@gmail.com>
2025-12-16 14:36:45 +0100

    feat:ms-gateway: Ajout de l'api gateway et routage initial du service Paitent

---
+++ ms-patient/ms-patient/${project.build.directory}/classes/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO patient (last_name, first_name, birthdate, genre, address, phone_number) VALUES
+                                                                                         ('TestNone', 'Test', '1966-12-31', 'FEMININ', '1 Brookside St', '100-222-3333'),
+                                                                                         ('TestBorderline', 'Test', '1945-06-24', 'MASCULIN', '2 High St', '200-333-4444'),
+                                                                                         ('TestInDanger', 'Test', '2004-06-18', 'MASCULIN', '3 Club Road', '300-444-5555'),
+                                                                                         ('TestEarlyOnset', 'Test', '2002-06-28', 'FEMININ', '4 Valley Dr', '400-555-6666');
