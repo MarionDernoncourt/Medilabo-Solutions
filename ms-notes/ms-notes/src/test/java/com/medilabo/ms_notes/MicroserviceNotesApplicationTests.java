4b53214099143ab38093ff166ed18b8045807588
MarionDernoncourt <marion.dernon@gmail.com>
2026-01-19 15:03:50 +0000

    feat(ms-notes): initialisation du microservice notes, Eureka et Gateway

---
+++ ms-notes/ms-notes/src/test/java/com/medilabo/ms_notes/MicroserviceNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.ms_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
